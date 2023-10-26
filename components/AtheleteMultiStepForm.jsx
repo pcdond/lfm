5926c0694840f83af02f7b8ef4e58210f2aca46d
pcdond <pcdond.multimedia@gmail.com>
2023-10-26 07:55:54 +0530

    wip

--- components/AtheleteMultiStepForm.jsx
+++ components/AtheleteMultiStepForm.jsx
@@ -1,19 +1,94 @@
-import React from 'react'
-import PersonalInfo from './PersonalInfo'
+"use client"
+import React, { useState } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import PersonalInfo from './PersonalInfo';
 import EducationBankInfo from './EducationBankInfo'
 import OtherInfo from './OtherInfo'
 import PreviewFinish from './PreviewFinish'
 
 const AtheleteMultiStepForm = () => {
+  const validationSchema = Yup.object().shape({
+    // Add validation schema for your entire form here
+  });
+
+  const initialValues = {
+    // Define your initial form values here
+  };
+
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = [
+    { component: <PersonalInfo/>, label: '1Account Setup' },
+    { component: <EducationBankInfo />, label: '2Social Profiles' },
+    { component: <OtherInfo />, label: '3Personal Details' },
+    { component: <PreviewFinish />, label: '4Personal Details' },
+  ];
+
+  const isLastStep = currentStep === steps.length - 1;
+
+  const handleNext = () => {
+    if (isLastStep) {
+      // Handle form submission here
+    } else {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
   return (
-    <>
-    <PersonalInfo/>
-    <EducationBankInfo/>
-    <OtherInfo/>
-    <PreviewFinish/>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        // Handle form submission here
+      }}
+    >
+      {() => (
+        <Form>
+          <h1 className="text-lg font-bold text-gray-700 leading-tight text-center mt-12 mb-5">
+            Form Wizard - Multi Step Form
+          </h1>
+          <div className="p-12 shadow-md rounded-2xl bg-white mx-auto border-solid border-2 border-gray-100 mb-8">
+            {/* Step Indicators */}
+            <div className="form-header flex gap-3 mb-4 text-xs text-center">
+              {steps.map((step, index) => (
+                <span
+                  key={index}
+                  className={`stepIndicator flex-1 pb-8 relative ${index === currentStep ? 'active' : ''}`}
+                >
+                  {step.label}
+                </span>
+              ))}
+            </div>
 
-    </>
-  )
-}
+            {/* Current Step Component */}
+            {steps[currentStep].component}
 
-export default AtheleteMultiStepForm
+            {/* Previous / Next buttons */}
+            <div className="form-footer flex gap-3">
+              <button
+                type="button"
+                className="prev-button"
+                onClick={handlePrevious}
+                style={{ display: currentStep === 0 ? 'none' : 'inline' }}
+              >
+                Previous
+              </button>
+              <button type="button" className="next-button" onClick={handleNext}>
+                {isLastStep ? 'Submit' : 'Next'}
+              </button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default AtheleteMultiStepForm;
--- components/PersonalInfo.jsx
+++ components/PersonalInfo.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const PersonalInfo = () => {
-  return (
-    <div>PersonalInfo</div>
-  )
-}
+  const initialValues = {
+    // Initialize your form fields here
+    first_name: '',
+    middle_name: '',
+    last_name: '',
+    mother_full_name: '',
+    mother_profession: '',
+    father_full_name: '',
+    father_profession: '',
+    Gender: '',
+    userDateofBirth: '',
+  };
 
-export default PersonalInfo
+  const validationSchema = Yup.object().shape({
+    // Define validation rules using Yup
+    first_name: Yup.string().required('Please enter full name'),
+    // Add validation rules for other fields
+  });
+
+  const onSubmit = (values) => {
+    // Handle form submission here
+    console.log(values);
+  };
+
+  return (
+    <div className="user-registration">
+      {/* <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form className="flex flex-wrap justify-around"> */}
+          {/* Full Name Field */}
+          <div className="md:w-full pr-4 pl-4 sm:w-1/2 pr-4 pl-4 w-full">
+            <div className="mb-4">
+              <label>
+                Full Name<i className="mandatory_icon"></i>
+              </label>
+              <Field
+                type="text"
+                name="first_name"
+                placeholder="Enter Full Name"
+                className="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded focus-error txtOnly sanitization"
+              />
+              <ErrorMessage
+                name="first_name"
+                component="span"
+                className="field-validation-valid error"
+              />
+            </div>
+          </div>
+          {/* Add similar Field components for other form fields */}
+        {/* </Form>
+      </Formik> */}
+    </div>
+  );
+};
+
+export default PersonalInfo;
